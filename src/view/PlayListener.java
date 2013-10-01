e07c0785b2de06201f2a63e5384aff8d4bbd4398
Lorenzo Affetti <affox90@gmail.com>
2013-10-01 18:31:37 +0200

    play functionality works

--- src/view/PlayListener.java
+++ src/view/PlayListener.java
@@ -3,23 +3,37 @@ package view;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.Timer;
+
 public class PlayListener implements ActionListener {
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
+	@Override
+	public void actionPerformed(ActionEvent e) {
 
-	ViewManager viewManager = ViewManager.getManager();
-	GameGrid grid = viewManager.getGrid();
+		// ViewManager viewManager = ViewManager.getManager();
+		// GameGrid grid = viewManager.getGrid();
+		//
+		// viewManager.setPlaying(true);
+		//
+		// if (grid.isFirstRun()) {
+		// grid.removeGridListeners();
+		// grid.setInitialConfiguration();
+		// }
+		//
+		// for(int i = 0; i<1000; i++){
+		// try {
+		// Thread.sleep(500);
+		// grid.runEpoch();
+		// } catch (InterruptedException e1) {
+		// // TODO Auto-generated catch block
+		// e1.printStackTrace();
+		// }
+		// }
 
-	viewManager.setPlaying(true);
+		StepListener stepper = new StepListener();
+		int delay = 300;
 
-	if (grid.isFirstRun()) {
-	    grid.removeGridListeners();
-	    grid.setInitialConfiguration();
+		new Timer(delay, stepper).start();
 	}
-
-	// TODO play
-
-    }
 
 }
