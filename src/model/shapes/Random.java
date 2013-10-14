49e59c16bf1e61a136a1e4b7c976686a2fbfe16d
Lorenzo Affetti <affox90@gmail.com>
2013-10-14 15:19:10 +0200

    random allright

--- src/model/shapes/Random.java
+++ src/model/shapes/Random.java
@@ -9,28 +9,31 @@ import view.JGrid;
 
 public class Random implements Shape {
 
-    @SuppressWarnings("unchecked")
-    @Override
-    public Set<Position> makeShape(Position start) {
-	Turtle turtle = new WorldTurtle(new Position(0, 0));
-	java.util.Random r = new java.util.Random();
-	int rows = JGrid.ROWS;
-	int columns = JGrid.COLUMNS;
+	@SuppressWarnings("unchecked")
+	@Override
+	public Set<Position> makeShape(Position start) {
+		Turtle turtle = new WorldTurtle(new Position(0, 0));
+		int rows = JGrid.ROWS;
+		int columns = JGrid.COLUMNS;
 
-	for (int i = 0; i < rows; i++) {
-	    for (int j = 0; j < columns; j++) {
-		boolean b = r.nextBoolean();
-		if (b) {
-		    turtle.penDown().penUp().right();
-		} else {
-		    turtle.right();
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				boolean b = nextBool();
+				if (b) {
+					turtle.penDown().penUp().right();
+				} else {
+					turtle.right();
+				}
+			}
+
+			turtle.down().left().times(columns);
 		}
-	    }
 
-	    turtle.down().left().times(columns - 1);
+		return (Set<Position>) turtle.getDraft();
 	}
 
-	return (Set<Position>) turtle.getDraft();
-    }
+	private boolean nextBool() {
+		return Math.random() < 0.2;
+	}
 
 }
