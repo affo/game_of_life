c86bb95deb0c4b5aa0451f2ebf552b77f7a1ba36
Lorenzo Affetti <affox90@gmail.com>
2013-10-14 14:22:19 +0200

    random sample added

---
+++ src/model/shapes/Random.java
@@ -0,0 +1,35 @@
+package model.shapes;
+
+import java.util.Set;
+
+import model.Position;
+import model.Turtle;
+import model.WorldTurtle;
+
+public class Random implements Shape {
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Set<Position> makeShape(Position start) {
+		Turtle turtle = new WorldTurtle(new Position(0, 0));
+		java.util.Random r = new java.util.Random();
+		int rows = 25;
+		int columns = 50;
+
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				boolean b = r.nextBoolean();
+				if(b){
+					turtle.penDown().penUp().right();
+				}else{
+					turtle.right();
+				}
+			}
+			
+			turtle.down().left().times(columns - 1);
+		}
+
+		return (Set<Position>) turtle.getDraft();
+	}
+
+}
